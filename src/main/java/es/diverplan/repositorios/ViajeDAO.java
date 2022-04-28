7b7cb792608a04ae2c3f32140bd64d652a1eab3d
JuanmaRamirez <jmrc87@hotmail.com>
2022-04-28 16:02:32 +0200

    Personalizando endpoints (métodos JPA)

--- src/main/java/es/diverplan/repositorios/ViajeDAO.java
+++ src/main/java/es/diverplan/repositorios/ViajeDAO.java
@@ -1,11 +1,18 @@
 package es.diverplan.repositorios;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import es.diverplan.entidades.ViajeConId;
 
 @RepositoryRestResource(path = "viajes", itemResourceRel = "viaje", collectionResourceRel = "viajes")
 public interface ViajeDAO extends JpaRepository<ViajeConId, Long> {
+	
+	@RestResource(path = "por-precio")
+	List<ViajeConId> findByPrice(@Param("precio") float precio);
 
 }
