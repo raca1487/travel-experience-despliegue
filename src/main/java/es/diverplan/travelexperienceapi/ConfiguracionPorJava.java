6975529e040fa01c8e7fa617daf8035421a6a874
JuanmaRamirez <jmrc87@hotmail.com>
2022-05-18 18:02:51 +0200

    Agregando CORS Filter

--- src/main/java/es/diverplan/travelexperienceapi/ConfiguracionPorJava.java
+++ src/main/java/es/diverplan/travelexperienceapi/ConfiguracionPorJava.java
@@ -1,9 +1,15 @@
 package es.diverplan.travelexperienceapi;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -24,6 +30,19 @@ public class ConfiguracionPorJava {
 		mapper.addMixIn(ValoracionConId.class, Mixins.ValoracionConId.class);
 
 		return mapper;
+	}
+	
+	@Bean
+	CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("*"));
+		config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+		config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+		source.registerCorsConfiguration("/**", config);
+
+		return new CorsFilter(source);
 	}
 
 }
